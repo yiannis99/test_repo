907da8c217b24f908fdfc4d9fe404f7fb29c3fdb
Yiannis Yiangou <yiangou@icon.cy>
2024-11-27 15:23:23 +0200

    added basepath

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
-  reactStrictMode: true,
+	basePath: isProd ? '/test_repo' : '',
+	reactStrictMode: true,
 };
 
 export default nextConfig;
